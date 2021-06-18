187123c0dbb5e398fa6da90cc13a4c5b0eebcc24
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-06-18 08:56:59 +0530

    Add files via upload

---
+++ GeeksforGeeks/Array/Triplet_Sum_in_Array.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+        sort(A,A+n);
+        for(int i=0;i<n-2;i++){
+            int j=i+1,k=n-1;
+            int s= X-A[i];
+            while(j<k){
+                if(A[j]+A[k]==s)
+                    return 1;
+                else if(A[j]+A[k]<s)
+                    j++;
+                else
+                    k--;
+            }
+        }
+    return 0;
+    }
+
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+  // } Driver Code Ends
