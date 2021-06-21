10b612a8a659a564d8ee937c1e966d4b124314b2
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-06-21 12:10:57 +0530

    Create 74_Search_a_2D_Matrix.cpp

---
+++ Leetcode/Matrix/74_Search_a_2D_Matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(!matrix.size())
+            return false;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        int l=0;
+        int h=(n*m)-1;
+        while(l<=h){
+            int mid=((l+(h-l)/2));
+            if(matrix[mid/m][mid%m]==target)
+                     return true;
+            else if(matrix[mid/m][mid%m]<target)
+                     l=mid+1;
+            else
+                     h=mid-1;
+        }
+        return false;
+    }
+};
