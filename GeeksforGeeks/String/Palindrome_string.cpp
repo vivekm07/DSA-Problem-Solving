33592be991d3460f7b5878927c6d4e7b68f715c3
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-06-24 09:20:54 +0530

    Create Palindrome_string.cpp

---
+++ GeeksforGeeks/String/Palindrome_string.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPlaindrome(string S)
+	{
+	    // Your code goes here
+	    int l=0;
+	    int r=S.length()-1;
+	    while(l<r){
+	        if(S[l++]!=S[r--]){
+	            return 0;
+	        }
+	    }
+	    return 1;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPlaindrome(s) << "\n";
+   	}
+
+    return 0;
+}  // } Driver Code Ends
