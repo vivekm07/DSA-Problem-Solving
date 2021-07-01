9f996ba33227a9b49795882460a9034eb1dca27b
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-07-01 22:27:32 +0530

    Add files via upload

---
+++ GeeksforGeeks/String/Min_Number_of_Flips.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int minFlips (string s);
+int32_t main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+
+        cout << minFlips (s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+// } Driver Code Ends
+
+
+int minFlips (string S)
+{
+    // your code here
+    int cnt=0;
+	    string s1="";
+	    string s2="";
+	    for(int i=0;i<S.length();i++)
+	    {
+	        if(i%2==0)
+	        {
+	            s1+="0";
+	            s2+="1";
+	        }
+	        else
+	        {
+	            s1+="1";
+	            s2+="0";
+	        }
+	    }
+	    int cnt1=0;
+	    int cnt2=0;
+	    for(int i=0;i<S.length();i++)
+	    {
+	        if(S[i]!=s1[i])
+	            cnt1++;
+	        if(S[i]!=s2[i])
+	            cnt2++;
+	    }
+	    return min(cnt1,cnt2);
+}
