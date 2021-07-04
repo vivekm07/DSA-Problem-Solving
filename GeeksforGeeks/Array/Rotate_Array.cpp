1774578f401138fbd1ed73b03b70b169cdfb84a2
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-07-05 00:42:14 +0530

    Create Rotate_Array.cpp

---
+++ GeeksforGeeks/Array/Rotate_Array.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+    
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void reverseArr(int arr[], int s, int e){
+        while(s<e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+    }
+    void rotateArr(int arr[], int d, int n){
+        // code here
+        if(d==0)
+            return;
+        d=d%n;
+        reverseArr(arr,0,d-1);
+        reverseArr(arr,d,n-1);
+        reverseArr(arr,0,n-1);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
