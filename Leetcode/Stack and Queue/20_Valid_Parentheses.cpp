304bc27c9b1fc6c249543be60134aa90bf415c02
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-07-11 18:47:02 +0530

    Create 20_Valid_Parentheses.cpp

---
+++ Leetcode/Stack and Queue/20_Valid_Parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(st.empty() || c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else{
+                if(st.empty())
+                    return false;
+                else{
+                    if(c==')' && st.top()!='(')
+                        return false;
+                    if(c=='}' && st.top()!='{')
+                        return false;
+                    if(c==']' && st.top()!='[')
+                        return false;
+                    else
+                        st.pop();
+                }
+            }
+        }
+        return st.empty();
+    }
+};
