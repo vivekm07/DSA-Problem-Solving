2069370acc8438f04577db0cd6cf8aa0c2a333b3
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-07-19 23:40:29 +0530

    Update Row_with_max_1s.cpp

--- GeeksforGeeks/Matrix/Row_with_max_1s.cpp
+++ GeeksforGeeks/Matrix/Row_with_max_1s.cpp
@@ -1,18 +1,48 @@
-int r = 0;              // keeps track of row; starts at first row             
-        int c = m-1;            // keeps track of column; starts at last column
-        int max_row_index=-1;   // keeps track of result row index
-        
-        // starting from top right corner
-        // go left if you encounter 1
-        // do down if you encounter 0
-        while(r<n && c>=0)
-        {
-            if(arr[r][c]==1)
-            {
-                max_row_index = r;
-                c--;
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	        // code here
+	        int r = 0;              
+                int c = m-1;            
+                int row=-1;   
+                while(r<n && c>=0)
+                {
+                        if(arr[r][c]==1)
+                        {
+                        row = r;
+                        c--;
+                }
+                else
+                        r++;
+                }
+                return row;
+	}
+	
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector< vector<int> > arr(n,vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin>>arr[i][j];
             }
-            else
-                r++;
         }
-        return max_row_index;
+        Solution ob;
+        auto ans = ob.rowWithMax1s(arr, n, m);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
