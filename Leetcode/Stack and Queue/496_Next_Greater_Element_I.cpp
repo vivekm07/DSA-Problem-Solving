59741f32322b2ead7e4b12985ff72f8048e86556
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-07-21 22:54:32 +0530

    Update 496_Next_Greater_Element_I.cpp

--- Leetcode/Stack and Queue/496_Next_Greater_Element_I.cpp
+++ Leetcode/Stack and Queue/496_Next_Greater_Element_I.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-     int findAddNext(int x, vector<int> v){
-        auto it=find(v.begin(),v.end(),x);
-        int t=it-v.begin(); 
-        for(int i=t;i<v.size();i++){
-            if(v[i]>x) 
-                return v[i];
+     int findAddNext(int x, vector<int> v1){
+        auto it=find(v1.begin(),v1.end(),x);
+        int t=it-v1.begin(); 
+        for(int i=t;i<v1.size();i++){
+            if(v1[i]>x) 
+                return v1[i];
         }
         return -1;
     }
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2){
-        vector<int> v;
+        vector<int> v1;
         for(int i=0;i<nums1.size();i++)
-            v.push_back(findAddNext(nums1[i],nums2));
+            v1.push_back(findAddNext(nums1[i],nums2));
         return v;
     }
 };
