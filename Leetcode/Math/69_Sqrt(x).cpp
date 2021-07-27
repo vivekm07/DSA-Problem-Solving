5e7e0a466a5d185dc2bc1dc40469ed330f1f0f20
Vivek Majithia <48577858+vivekm07@users.noreply.github.com>
2021-07-27 22:56:26 +0530

    Update 69_Sqrt(x).cpp

--- Leetcode/Math/69_Sqrt(x).cpp
+++ Leetcode/Math/69_Sqrt(x).cpp
@@ -3,7 +3,7 @@ public:
     int mySqrt(int x) {
         if(x==1)
             return x; 
-        for(unsigned int i=1;i<=x/2;i++){
+        for(int i=1;i<=x/2;i++){
             if((i*i == x)|| (i*i <x) && ((i+1)*(i+1)>x))
                 return i;
         }
